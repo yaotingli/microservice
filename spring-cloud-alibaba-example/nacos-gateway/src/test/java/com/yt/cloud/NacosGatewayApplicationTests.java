5c75c042fb0ab7120c3754f6d92426a6f0f45952
469228402@qq.com <yaotingli>
2023-03-22 16:09:23 +0800

    gateway网关

---
+++ spring-cloud-alibaba-example/nacos-gateway/src/test/java/com/yt/cloud/NacosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
